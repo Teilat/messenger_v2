cc6a927f05be79009d85101aac415c209b2752c1
shura <shurathegod@gmail.com>
2023-12-07 18:01:06 +0500

    routes started

--- webapi/models/chats.go
+++ webapi/models/chats.go
@@ -1,1 +1,35 @@
 package models
+
+import (
+	"github.com/google/uuid"
+	"time"
+)
+
+type User struct {
+	ID           uuid.UUID `json:"id"`
+	Email        string    `json:"email"`
+	Username     string    `json:"username"`
+	CustomName   string    `json:"name"`
+	Bio          string    `json:"bio"`
+	PasswordHash string    `json:"password"`
+	LastOnline   time.Time `json:"time_stamp"`
+	Image        []byte    `json:"image"`
+}
+type UserLogin struct {
+	Email        string `json:"email"`
+	PasswordHash string `json:"password"`
+}
+
+// PROSTI MENYA!
+
+type Chat struct {
+	Message     string    `json:"message"`
+	MessageDate time.Time `json:"message_date"`
+	Name        string    `json:"chat_name"`
+	Users       []*User   `json:"users"`
+}
+type CreateChat struct {
+	Name string `json:"chat_name"`
+	//will think of it later потому что я таво рот шатала!!! надо придумать как умно получать юзер 1 и юзер 2!!!
+	//у себя я через jwt токен который хранит идентити получаю реквестера, но пока не придумал как добавляемого извлечь
+}
--- webapi/webapi.go
+++ webapi/webapi.go
@@ -1,4 +1,71 @@
 package webapi
 
+import (
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+	"log"
+	"messenger_v2/webapi/models"
+)
+
 type Webapi interface {
+	registerHandler() gin.HandlerFunc
+	loginHandler() gin.HandlerFunc
+	createChat() gin.HandlerFunc
+	run()
+}
+type webapi struct {
+	log *log.Logger
+	db  *mongo.Database
+}
+
+func (w webapi) registerHandler() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var user *models.User
+		c.BindJSON(&user)
+		c.JSON(200, &user)
+	}
+}
+
+func (w webapi) loginHandler() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var user *models.UserLogin
+		c.BindJSON(&user)
+		c.JSON(200, &user)
+	}
+}
+
+func (w webapi) createChat() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var create *models.CreateChat
+		c.BindJSON(&create)
+		c.JSON(200, &create)
+	}
+}
+
+func (w webapi) run() {
+	router := gin.Default()
+
+	auth := router.Group("")
+	auth.POST("sign-up", w.registerHandler())
+	auth.POST("sign-in", w.loginHandler())
+
+	chat := router.Group("/chat")
+	chat.POST("/add-chat", nil)
+
+	message := router.Group("/message")
+	message.POST("/add-message", nil)
+
+	group := router.Group("/group")
+	group.POST("/add-chat", nil)
+
+	err := router.Run("8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+func mneLen(db *mongo.Database, logger *log.Logger) Webapi {
+	return webapi{
+		log: logger,
+		db:  db,
+	}
 }
